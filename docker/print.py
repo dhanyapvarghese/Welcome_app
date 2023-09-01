b760fc93660b41f4eca2e6879d8b949949efa3bd
dhanyapvarghese <43697021+dhanyapvarghese@users.noreply.github.com>
2023-09-01 22:33:40 +0530

    Update print.py

--- docker/print.py
+++ docker/print.py
@@ -1,1 +1,13 @@
-print ("Welcome to 2023");
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Flask inside Docker!!"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 80))
+    app.run(debug=True,host='0.0.0.0',port=port)
