d49b6a8520cdc4158433984fccf7f98a783c5fc2
dhanyapvarghese <dhanyapvarghese@gmail.com>
2023-09-06 15:45:40 +0530

    deployment modified

--- HelloWorldApp.web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace HelloWorldApp.web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
