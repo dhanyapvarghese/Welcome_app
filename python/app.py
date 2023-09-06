7de975306ec4ffdbcd5874a1944a3d96d9899377
dhanyapvarghese <43697021+dhanyapvarghese@users.noreply.github.com>
2023-09-06 15:21:34 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -6,7 +6,7 @@ def hello():
     return "Welcome to 2023!"
 
 if __name__ == "__main__":
-    app.run(debug=True,host='127.0.0.1',port=80)
+    app.run(debug=True,host='0.0.0.0',port=5000)
 
 # Why you should run it at 0.0.0.0
 # https://stackoverflow.com/questions/30323224/deploying-a-minimal-flask-app-in-docker-server-connection-issues
